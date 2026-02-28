f1a6d7599744142871dae91b3cfed6f6fd7bfc35
Steven Bailey <sraybailey@gmail.com>
2026-02-28 16:14:49 -0500

    Decluttered unneeded config blocks
    Following recommended practice, minimizing or eliminating usage of
    config blocks in plugin definitions, primarily with keys and opts
    blocks.

--- lua/SrBlackVoid/plugins/lazygit.lua
+++ lua/SrBlackVoid/plugins/lazygit.lua
@@ -10,24 +10,18 @@ return {
 			"LazyGitFilterCurrentFile",
 		},
 		keys = { --Only if lazy-loading (non-telescope) 
-			{
-				'<leader>Gg', ':LazyGit<CR>', desc = "Lazy[G]it"
-			},
-			{
-				'<leader>Gc', ':LazyGitCurrentFile<CR>', desc = "Lazy[G]it: [C]urrent File Repo"
-			},
-			{
-				'<leader>Gf', ':LazyGitFilterCurrentFile<CR>', desc = "Lazy[G]it: [F]ilter by current file"
-			}
+			{ '<leader>Gg', ':LazyGit<CR>', desc = "Lazy[G]it" },
+			{ '<leader>Gc', ':LazyGitCurrentFile<CR>', desc = "Lazy[G]it: [C]urrent File Repo" },
+			{ '<leader>Gf', ':LazyGitFilterCurrentFile<CR>', desc = "Lazy[G]it: [F]ilter by current file" }
 		},
 		dependencies = {
 			-- "nvim-telescope/telescope.nvim",
 			"nvim-lua/plenary.nvim",
 		},
-		config = function()
-			-- require("telescope").load_extension("lazygit")
+		init = function()
+			-- Set globals at startup, before plugin loads
 			vim.g.lazygit_floating_window_use_plenary = 1
 			vim.g.lazygit_floating_window_winblend = 1
-		end
-  }
+		end,
+	}
 }
--- lua/SrBlackVoid/plugins/todo.lua
+++ lua/SrBlackVoid/plugins/todo.lua
@@ -4,12 +4,9 @@ return {
 		dependencies = {
 			"nvim-lua/plenary.nvim"
 		},
-		--[[ keys = {
+		keys = {
 			{'<leader>wt',':TodoTelescope<CR>', desc = "[W]orkspace [T]oDo"}
-		}, ]]
+		},
 		opts = {},
-		init = function()
-			vim.keymap.set('n', '<leader>wt',':TodoTelescope<CR>', { desc = "[W]orkspace [T]oDo" })
-		end
 	}
 }

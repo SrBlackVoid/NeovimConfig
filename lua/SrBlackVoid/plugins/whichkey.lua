3d4946b0ed1f14b77cb83094e9df8918725133ae
Steven Bailey <sraybailey@gmail.com>
2026-03-01 17:10:43 -0500

    Fixed up WhichKey config, lazy-load

--- lua/SrBlackVoid/plugins/whichkey.lua
+++ lua/SrBlackVoid/plugins/whichkey.lua
@@ -1,21 +1,18 @@
 return {{
 	"folke/which-key.nvim",
-	config = function()
-		require("which-key").setup({
-			notify = false,
-		})
-
-		--Document existing keychains
-		require('which-key').register {
-			['<leader>c'] = { name = '[C]ode', _ = 'which_key_ignore' },
-			['<leader>d'] = { name = '[D]ocument', _ = 'which_key_ignore' },
-			['<leader>dd'] = { name = '[D]ocument [D]iff', _ = 'which_key_ignore' },
-			['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
-			['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
-			['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
-			['<leader>g'] = { name = '[g]oto', _ = 'which_key_ignore' },
-			['<leader>G'] = { name = '[G]it', _ = 'which_key_ignore' },
-			['<leader>t'] = { name = '[T]erminal', _ = 'which_key_ignore' }
+	event = "VeryLazy",
+	opts = {
+		notify = false,
+		spec = {
+			{ "<leader>c", group = "[C]ode" },
+			{ "<leader>d", group = "[D]ocument" },
+			{ "<leader>dd", group = "[D]ocument [D]iff" },
+			{ "<leader>r", group = "[R]ename" },
+			{ "<leader>s", group = "[S]earch" },
+			{ "<leader>w", group = "[W]orkspace" },
+			{ "<leader>g", group = "[g]oto" },
+			{ "<leader>G", group = "[G]it" },
+			{ "<leader>t", group = "[T]erminal" }
 		}
-	end
+	},
 }}

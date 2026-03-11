9c32a66d1679da070210b5e39ac209941b1abf03
Steven Bailey <sraybailey@gmail.com>
2026-03-11 12:40:12 -0400

    Lazy-load todo-comments on buffer open

--- lua/SrBlackVoid/plugins/todo.lua
+++ lua/SrBlackVoid/plugins/todo.lua
@@ -3,6 +3,7 @@ return {{
 	dependencies = {
 		"nvim-lua/plenary.nvim"
 	},
+	event = { "BufReadPre", "BufNewFile" },
 	keys = {
 		{'<leader>wt',':TodoTelescope<CR>', desc = "[W]orkspace [T]oDo"}
 	},
